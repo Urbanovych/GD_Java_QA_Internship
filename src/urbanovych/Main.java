7d00e43412566fcc3c4865295d648da8162f588a
Urbanovych <98735406+Urbanovych@users.noreply.github.com>
2022-07-18 23:34:52 +0300

    1 to 12

--- src/urbanovych/Main.java
+++ src/urbanovych/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // System.out.println("Hello in second branch");
         System.out.println("Hello in second branch (again)");
-	System.out.println("1");
+	System.out.println("12");
 	    // System.out.println("Hello world");
 	    
     }
