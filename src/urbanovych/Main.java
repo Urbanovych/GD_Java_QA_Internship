67067f55e327ee9d9ee4d4e70c92b583a558473c
Urbanovych <urbanovych.qa@gmail.com>
2022-07-18 21:35:17 +0300

    Add 2

--- src/urbanovych/Main.java
+++ src/urbanovych/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("Hello in second branch");
         System.out.println("Hello in second branch (again)");
 	System.out.println("1");
+        System.out.println("2");
 	    // System.out.println("Hello world");
 	    
     }
